6056f665ce6d7f5b5f7a8ca67b009d4f9512d6a0
Balazs Garda <gardab84@gmail.com>
2023-01-05 17:22:32 +0100

    Configure React app for deployment to GitHub Pages

---
+++ README.md
@@ -0,0 +1,49 @@
+# Frontend Mentor - Calculator app solution
+
+This is a solution to the [Calculator app challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/calculator-app-9lteq5N29). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+- [Acknowledgments](#acknowledgments)
+
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- See the size of the elements adjust based on their device's screen size
+- Perform mathmatical operations like addition, subtraction, multiplication, and division
+- Adjust the color theme based on their preference
+- **Bonus**: Have their initial theme preference checked using `prefers-color-scheme` and have any additional changes saved in the browser
+
+### Screenshot
+
+![](./screenshot.jpg)
+
+
+### Links
+
+- Live Site URL: https://garda-b.github.io/calculator/
+
+## My process
+
+Many calculators are built using the eval() function or the Function constructor. As this is not considered best practice, I created mine with a totally original logic. It's by far not perfect and rather rudimentary, but it works.  
+
+### Built with
+
+- React.JS
+
+## Acknowledgments
+
+I used this guy's tutorial for the themes:
+https://www.youtube.com/watch?v=Yfd6N0G1Sq0 
+
+
---
+++ src/components/calculator.jsx
@@ -0,0 +1,198 @@
+import { useState } from 'react';
+import { useEffect } from 'react';
+
+
+function Calculator() {
+
+    const [num, setNum] = useState("")
+    const [result, setResult] = useState([0, false])
+    const [operator, setOperator] = useState("")
+
+    const [toggleButton, setToggleButton] = useState("circle-one")
+    const [theme, setTheme] = useState("theme-one")
+
+
+
+    const handleOperator = (e) => {
+
+
+        if (num != "" && operator == "+" && result[1] == false) {
+            setResult([result[0] + +num, false])
+        } else if (num != "" && operator == "*" && result[1] == false) {
+            setResult([result[0] * +num, false])
+        } else if (num != "" && operator == "/" && result[1] == false) {
+            setResult([result[0] / +num, false])
+        } else if (num != "" && operator == "-" && result[1] == false) {
+            setResult([result[0] - +num, false])
+        } else if (num != "" && result[1] == true) {
+            setResult([result[0] - result[0] + +num, false])
+
+        } else if (result[0] == 0 && result[1] == false) {
+            setResult([result[0] + +num, false])
+        }
+
+        switch (e.target.name) {
+            case "plus":
+
+                setOperator("+")
+
+                setNum("")
+
+                break
+
+            case "minus":
+
+
+
+                setOperator("-")
+                setNum("")
+
+                break
+
+            case "multiply":
+
+
+                setOperator("*")
+                setNum("")
+
+                break
+
+            case "divide":
+
+
+                setOperator("/")
+                setNum("")
+        }
+
+    }
+
+    const equals = () => {
+
+        if (operator == "+") {
+            setNum("")
+            setResult([result[0] + +num, true])
+
+
+        } else if (operator == "-") {
+            setNum("")
+            setResult([result[0] - +num, true])
+
+        } else if (operator == "*") {
+            setNum("")
+            setResult([result[0] * +num, true])
+        }
+
+        else if (operator == "/") {
+            setNum("")
+            setResult([result[0] / +num, true])
+        }
+
+    }
+
+    const handleDelete = () => {
+
+        setNum(prevelement => prevelement.slice(0, prevelement.length - 1))
+        
+    }
+
+
+    const numberformatted = (
+
+
+        num.includes('.') ? new Intl.NumberFormat().format(num.split(".")[0]) + "." + num.split(".")[1] : new Intl.NumberFormat().format(num)
+
+    )
+
+    const resultformatted = (
+        result[0].toString().includes('.') ? result[0] : new Intl.NumberFormat().format(result[0])
+
+    )
+
+
+    const handleToggle = () => {
+
+        if (toggleButton == "circle-one") {
+            setToggleButton("circle-two")
+            setTheme("theme-two")
+        } else if (toggleButton == "circle-two") {
+            setToggleButton("circle-three")  
+            setTheme("theme-three")
+        } else {
+            setToggleButton("circle-one")
+            setTheme("theme-one")
+
+        }
+    }
+
+
+    useEffect(() => {
+            document.body.className = theme    
+
+    }, [toggleButton])
+
+
+    return (
+        <div className='wrapper'>
+
+            <div className='flex firstrow'>
+                <div>calc</div>
+                
+                <div className='theme smallerfont'>THEME</div>
+                <div>
+                    <ul className='flex smallerfont'>
+                        <li>1</li>
+                        <li>2</li>
+                        <li>3</li>
+                    </ul>
+                    
+                    
+                    <div onClick={handleToggle} className='toggle'><div  className={toggleButton}></div></div>
+                </div>
+
+                
+            </div>
+
+            <input className='display' type="text" readonly="readonly" name="number"  value={num ? numberformatted : resultformatted} onChange={(e) => setNum(+e.target.value)} ></input>
+
+
+            <div className='keypad'>
+
+            <div className='keypadrow'>
+                <button value={7} onClick={() => setNum(prevelement => prevelement + 7)}>7</button>
+                <button value={8} onClick={() => setNum(prevelement => prevelement + 8)}>8</button>
+                <button value={9} onClick={() => setNum(prevelement => prevelement + 9)}>9</button>
+                <button className="resetdel" onClick={handleDelete}>DEL</button>
+            </div>
+
+            <div className='keypadrow'>
+                <button value={4} onClick={() => setNum(prevelement => prevelement + 4)}>4</button>
+                <button value={5} onClick={() => setNum(prevelement => prevelement + 5)}>5</button>
+                <button value={6} onClick={() => setNum(prevelement => prevelement + 6)}>6</button>
+                <button name="plus" onClick={(e) => handleOperator(e)}>+</button>
+            </div>
+
+            <div className='keypadrow'>
+                <button value={1} onClick={() => setNum(prevelement => prevelement + 1)}>1</button>
+                <button value={2} onClick={() => setNum(prevelement => prevelement + 2)}>2</button>
+                <button value={3} onClick={() => setNum(prevelement => prevelement + 3)}>3</button>
+                <button name="minus" onClick={(e) => handleOperator(e)}>-</button>
+            </div>
+
+            <div className='keypadrow'>
+                <button onClick={() => setNum(prevelement => prevelement + ".")}>.</button>
+                <button value={0} onClick={() => setNum(prevelement => prevelement + 0)}>0</button>
+                <button name="divide" onClick={(e) => handleOperator(e)}>/</button>
+                <button name="multiply" onClick={(e) => handleOperator(e)}>x</button>
+            </div>
+
+            <div className='keypadrow'>
+                <button className='bottomrow resetdel' onClick={() => [setNum(""), setResult([0, false]), setOperator("")]}>RESET</button>
+                <button className='bottomrow equalsigncolor resetdel' onClick={equals}>=</button>
+            </div>
+
+            </div>
+        </ div>
+    )
+}
+
+export default Calculator;
